f9f9d7fd0a356685d45ba21225fee66b03ccd7a5
Eje Joshua <eje_joshua@yahoo.com>
2024-09-22 16:52:42 +0100

    modified auth server and create manifests

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,22 +1,19 @@
-from dataclasses import dataclass
-from hashlib import algorithms_available
-
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
 
-
 server = Flask(__name__)
 mysql = MySQL(server)
 
-# Config
+# config
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
-@server. route("/login", methods=["POST"])
+
+@server.route("/login", methods=["POST"])
 def login():
     auth = request.authorization
     if not auth:
@@ -34,41 +31,43 @@ def login():
         password = user_row[1]
 
         if auth.username != email or auth.password != password:
-            return "Invalid credentials", 401
+            return "invalid credentials", 401
         else:
-            return create_jwt(auth.username, os.environ.get ("JWT_SECRET"), True)
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
     else:
-        return "Invalid credentials", 401
+        return "invalid credentials", 401
 
 
-@server.route("/validate", method= ["POST" ])
+@server.route("/validate", methods=["POST"])
 def validate():
     encoded_jwt = request.headers["Authorization"]
+
     if not encoded_jwt:
-        return "Missing credentials", 401
+        return "missing credentials", 401
 
     encoded_jwt = encoded_jwt.split(" ")[1]
 
     try:
-        decoded = encoded_jwt.decode(
-            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"]
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
         )
     except:
-        return "Not authorized", 403
+        return "not authorized", 403
 
     return decoded, 200
 
 
-def create_jwt(username, secret, authz):
+def createJWT(username, secret, authz):
     return jwt.encode(
         {
             "username": username,
-            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
-            "iat": datetime.datetime.now(),
-            "admin":authz
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
         },
         secret,
-        algorithm="HS256"
+        algorithm="HS256",
     )
 
 
